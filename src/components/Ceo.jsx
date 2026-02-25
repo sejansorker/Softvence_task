8dc111d1e836a4892831f860c2c59d7d59043e7c
sejansorker <sejansorker@gmail.com>
2026-02-25 19:15:34 +0600

    ceo section complete

--- src/components/Ceo.jsx
+++ src/components/Ceo.jsx
@@ -9,6 +9,9 @@ const Ceo = () => {
                 <h2 className='text-[48px] pb-4 text-[#0A0915] font-normal font-main '>A message from our CEO</h2>
                 <p className='text-[16px] text-[#52525A] font-normal font-main '>With years of experience in the real estate industry, we are dedicated to helping individuals and families discover the perfect property that aligns with their lifestyle and needs.</p>
             </div>
+            <div className="flex justify-center pt-14 pb-30">
+                <img src={Ce} alt="" />
+            </div>
         </Container>
     </div>
   )
