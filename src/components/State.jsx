1f6f7270cff6e4a80bc47c342af1bcd732219f0b
sejansorker <sejansorker@gmail.com>
2026-02-27 21:50:12 +0600

    sate div rounded

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -5,7 +5,7 @@ const State = () => {
     return (
         <div className='bg-[url(/pagenation.png)] pt-[72px] pb-[127px] bg-cover bg-center bg-no-repeat '>
             <Container>
-                <div className="w-2/5 bg-white h-[691px]">
+                <div className="w-2/5 bg-white h-[691px] rounded-[16px]">
                    <div className="pt-12 px-10 pb-10">
                     <h3 className='text-[48px] text-[#0A0915] font-main font-normal pb-4 leading-[125%]'>Turning your real estate dreams true!</h3>
                     <p className='text-[#52525A] text-[18px] font-main font-normal pb-8'>Have questions or ready to take the next step? Whether you’re buying, selling, or just exploring options.</p>
