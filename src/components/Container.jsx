00488d7cc1a547ae043fbb445ff7a2c6176c7624
sejansorker <sejansorker@gmail.com>
2026-03-15 15:22:00 +0600

    comtainer fix

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({children}) => {
   return (
-    <div className='max-w-[1320px] mx-auto'>{children}</div>
+    <div className='max-w-[1320px] mx-auto lg:px-0 px-2'>{children}</div>
   )
 }
 
